9b76586529bb6327602a4e770691b40508f39efa
shallon <shallonmokeira@gmail.com>
2023-10-27 01:06:20 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -11,9 +11,23 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Author name is required.')
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError('Phone number must be a 10-digit number')
+        return value
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -26,6 +40,31 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary is not None and len(summary) >= 250:
+            raise ValueError('Summary must be at most 250 characters long')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ('Fiction', 'Non-Fiction'):
+            raise ValueError('Category must be "Fiction" or "Non-Fiction"')
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError('Title must contain at least one clickbait phrase')
+        return title
 
 
     def __repr__(self):
